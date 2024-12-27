c6e66aac96952abf6fb73bb4bfa6147cc4a01651
alirezatcp <alirezataaty02@gmail.com>
2024-12-27 09:12:15 -0500

    Add a simple FBV.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    if request.method == 'GET':
+        return Response({'message': 'Hello GET!!!'})
+
+    return Response({'message': "Hello POST!!!"})
