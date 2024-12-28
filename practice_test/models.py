9dcb5b90d97ffbd763be674fbeb4a6966f07e63c
alirezatcp <alirezataaty02@gmail.com>
2024-12-28 11:25:13 -0500

    Add our model.

--- practice_test/models.py
+++ practice_test/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def is_young(self):
+        return self.birth_date > datetime.date(1990,1,1)
+    
+    def is_old(self):
+        return self.birth_date < datetime.date(1960,1,1)
