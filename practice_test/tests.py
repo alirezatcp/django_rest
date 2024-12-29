195a151c7c153aeac2131ad0f0a85d9a3afc3d8e
alirezatcp <alirezataaty02@gmail.com>
2024-12-29 15:49:33 -0500

    Add about fixtures.

--- practice_test/tests.py
+++ practice_test/tests.py
@@ -8,7 +8,11 @@ from model_bakery import baker # fills all fields of model that we didnt gave. i
 
 
 class PersonTests(TestCase):
+
+    fixtures = ['person.json']
+    
     def setUp(self):
+        print(Person.objects.get(first_name='Alireza'))
         self.person = baker.make(Person, first_name='Firstname') # create an object from person that filled fields randomly.
         print(f'First name: {self.person.first_name}\nLast name: {self.person.last_name}\nBirth date {self.person.birth_date}') # show person detail
         Person.objects.create(first_name='Ali', last_name='Taghipour', birth_date= datetime.date(2000, 4, 12))
@@ -71,3 +75,15 @@ class APIViewTests(TestCase):
 # coverage report
 # to see result as html (after this command run htmlcov/index.html in browser to see results.):
 # coverage html
+
+
+# Fixtures
+# we can use fixtures to add objects to our model.
+# fixtures is json, xml or yaml files.
+# we can use them in tests or add them to database.
+# an example from fixtures is in practice_test/fixtures/person.json (if we change fixtures name we should change FIXTURE_DIRS in settings.py):
+# we added our fixtures in PersonTests.
+# to add fixtures to our database: python manage.py loaddata practice_test/fixtures/person.json
+# to create fixture from our database:  python manage.py dumpdata > practice_test/fixtures/dumped_data.json
+# or we can dump a table data:  python manage.py dumpdata table_name > practice_test/fixtures/dumped_data.json
+# or we can dump an app data:  python manage.py dumpdata app_name > practice_test/fixtures/dumped_data.json
