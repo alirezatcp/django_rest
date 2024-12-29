424d47fcec681ec4b895ddda3cdcba0a46bb83d3
alirezatcp <alirezataaty02@gmail.com>
2024-12-29 16:49:14 -0500

    Add about async.

--- practice_test/views.py
+++ practice_test/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
-# Create your views here.
+# about asyncs:
+
+from django.http import HttpResponse
+
+from .models import Person
+
+import asyncio
+from asgiref.sync import sync_to_async, async_to_sync
+
+
+# this is a sync view (all ORM is sync):
+def get_person(request, first_name):
+    person = Person.objects.get(first_name=first_name)
+    return HttpResponse(f'person: {person}')
+
+
+# this is async view (in async if something went wrong, it does another things and dont wait to end that first and it make works faster.):
+async def wait_2_seconds(request):
+    await asyncio.sleep(2)
+    return HttpResponse('Hi')
+
+
+# convert sync to async:
+async def get_person1(request, first_name):
+    # multi line
+    person_get = sync_to_async(Person.objects.get)
+    person = await person_get(first_name=first_name)
+
+    # one line
+    person = await sync_to_async(Person.objects.get)(first_name=first_name)
+
+    return HttpResponse(f'person: {person}')
+
+
+# or we can do it like this:
+@sync_to_async
+def person_get(first_name):
+    return Person.objects.get(first_name=first_name)
+
+async def get_person2(request, first_name):
+    person = await person_get(first_name=first_name)
+    return HttpResponse(f'person: {person}')
+
+
+# use async as sync
+@async_to_sync
+async def hello_message(name):
+    await asyncio.sleep(3)
+    return f'welcome {name}'
+
+def main():
+    msg = hello_message('mohammad')
+    print(msg)
