35828735da0e8d5e5d21e26f6dd2efda8ed6a8a6
alirezatcp <alirezataaty02@gmail.com>
2024-12-30 15:26:53 -0500

    Add about middlewares in django.

---
+++ rest/middleware.py
@@ -0,0 +1,29 @@
+from django.http.response import HttpResponseForbidden
+
+# middleware as func
+def blacklist_middleware(get_response): # django define get_response
+    def middleware(request):
+        client_ip = get_client_ip(request)
+        if client_ip in blacklist:
+            return HttpResponseForbidden()
+
+        response = get_response(request)
+        return response
+
+    return middleware
+
+
+# middleware as class
+class BlacklistMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response # django define get_response
+
+    def __call__(self, request):
+        client_ip = get_client_ip(request)
+        if client_ip in blacklist:
+            return HttpResponseForbidden
+
+        response = get_response(request)
+        return response
+
+# now we should add them in MIDDLEWARE in settings.
--- rest/settings.py
+++ rest/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # 'rest.middleware.BlacklistMiddleware',
+    # 'rest.middleware.blacklist_middleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
