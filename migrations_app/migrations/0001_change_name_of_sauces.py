6191e304b76d7b3d735e3dcf1543a897dafef9e2
alirezatcp <alirezataaty02@gmail.com>
2025-01-01 04:40:04 -0500

    Add a manual migration.

---
+++ migrations_app/migrations/0001_change_name_of_sauces.py
@@ -0,0 +1,45 @@
+
+from django.db import migrations, models
+from django.db.models import F, Value, CharField
+from django.db.models.functions import Concat, Replace
+
+
+from migrations_app.models import Cake
+
+
+def alter_names_of_sauces(apps,schema_editor):
+    '''A function to add ' Sauce' to end of all sauces in Cake objects.'''
+
+    Cake = apps.get_model('migrations_app', 'Cake') 
+
+    Cake.objects.annotate(
+        new_sauce_name = Concat(
+            F('sauce'), Value(' Sauce'), output_field=CharField()
+        )
+    ).update(sauce = F('new_sauce_name'))
+
+
+def reverse_alter_names_of_sauces(apps,schema_editor):
+    '''Reverse of above function.'''
+
+    Cake = apps.get_model('migrations_app', 'Cake')
+
+    Cake.objects.update(
+        sauce=Replace('sauce', text=Value(' Sauce'), replacement=Value(''))
+    )
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('migrations_app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            alter_names_of_sauces,
+            reverse_code=reverse_alter_names_of_sauces,
+        ),
+    ]
