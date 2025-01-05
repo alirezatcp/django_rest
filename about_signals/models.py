d6b7db8f7b680216896aa20170e8ac05d52ace92
alirezatcp <alirezataaty02@gmail.com>
2025-01-05 15:30:26 -0500

    Add model to our app.

--- about_signals/models.py
+++ about_signals/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    created_time = models.DateTimeField(auto_now=True)
